2680e0adba709f8bb73600c3a37d64b22505c2ee
Jacob Stewart <jacob.g.stewart@gmail.com>
2022-02-13 20:04:12 -0500

    setup: acting custom theme to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,30 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      sans: "Spartan, sans-serif",
+    },
+    fontSize: {
+      xs: [".6875rem", { lineHeight: "1.636", letterSpacing: "-.0144rem" }],
+      sm: [".75rem", { lineHeight: "1.25", letterSpacing: "-.0156rem" }],
+      md: ["1rem", { lineHeight: "1.5", letterSpacing: "-.05rem" }],
+      lg: ["1.25rem", { lineHeight: "1.1", letterSpacing: "-.0394rem" }],
+      xl: ["2rem", { lineHeight: "1.125", letterSpacing: "-.0625rem" }],
+    },
+    colors: {
+      purple: "#7C5DFA",
+      lightPurple: "#9277FF",
+      red: "#EC5757",
+      lightRed: "#9277FF",
+      darkOne: "#252945",
+      darkTwo: "#1E2139",
+      darkThree: "#141625",
+      darkFour: "#0C0E16",
+      lightOne: "#F8F8FB",
+      lightTwo: "#DFE3FA",
+      lightThree: "#7E88C3",
+      lightFour: "#888EB0",
+    },
   },
   plugins: [],
 };
