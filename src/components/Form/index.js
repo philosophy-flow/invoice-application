bbaee1a3024e7adb56c12719c4ac2bed76518a1f
Jacob Stewart <jacob.g.stewart@gmail.com>
2022-04-09 11:49:52 -0400

    style: removing unnecessary padding

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -78,7 +78,7 @@ export default function Form({ active = false, activeInvoice, setFormActive }) {
                   </h2>
                 </header>
 
-                <div className="overflow-scroll flex-1 -mx-[6.4%] px-[7%] scrollbar-hide">
+                <div className="overflow-scroll flex-1 scrollbar-hide">
                   <BillFrom errors={errors} touched={touched} />
                   <BillTo errors={errors} touched={touched} />
                   <MoreInfo errors={errors} touched={touched} />
