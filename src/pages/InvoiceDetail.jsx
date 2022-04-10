3c1acaaf4bf9b6c8d71ada888e7cadc2704048b5
Jacob Stewart <jacob.g.stewart@gmail.com>
2022-04-09 23:12:49 -0400

    reverting changes

--- src/pages/InvoiceDetail.jsx
+++ src/pages/InvoiceDetail.jsx
@@ -7,7 +7,6 @@ import DetailCard from "../components/DetailCard";
 import DeleteInvoice from "../components/DeleteInvoice";
 
 import { sampleData } from "../util/constants";
-import { scrollToTop } from "../util/helpers";
 
 function InvoiceDetail({ setFormActive, activeInvoice, setActiveInvoice }) {
   const { id, status } = activeInvoice;
@@ -26,15 +25,12 @@ function InvoiceDetail({ setFormActive, activeInvoice, setActiveInvoice }) {
   };
 
   const handleFormOpen = () => {
-    scrollToTop();
-    setTimeout(() => {
-      document.body.style.position = "fixed";
-      document.body.style.top = "0";
-      document.body.style.bottom = "0";
-      document.body.style.left = "0";
-      document.body.style.right = "0";
-      setFormActive(true);
-    }, 1000);
+    document.body.style.position = "fixed";
+    document.body.style.top = "0";
+    document.body.style.bottom = "0";
+    document.body.style.left = "0";
+    document.body.style.right = "0";
+    setFormActive(true);
   };
 
   return (
