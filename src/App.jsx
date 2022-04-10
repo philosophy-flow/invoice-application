8f215d88ad86cb5f5c5f93c5c73e860c4936cc05
Jacob Stewart <jacob.g.stewart@gmail.com>
2022-04-09 23:18:19 -0400

    Revert "testing no header"
    This reverts commit 612cc9e007189cef9c9dae4a07db9156a97f7cca.

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ export default function App() {
 
   return (
     <div className="bg-lightOne min-h-screen">
-      {/* <Header /> */}
+      <Header />
       <Pages
         setFormActive={setFormActive}
         invoices={invoices}
