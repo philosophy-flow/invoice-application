16bbdac74107ac5a8f7b757158873cfc47b60e1b
Jacob Stewart <jacob.g.stewart@gmail.com>
2022-06-04 12:21:42 -0400

    styles: adding custom checkbox styles

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -15,21 +15,21 @@ export default function Filter({
 
   return (
     <form onChange={handleOnChange}>
-      <div className="absolute top-full left-1/2 -translate-x-1/2 bg-white p-6 text-sm z-50 font-bold w-[140px] border-2 border-red">
+      <div className="absolute top-[120%] left-1/2 -translate-x-1/2 bg-white p-6 text-sm z-10 font-bold w-[140px] rounded-lg shadow-lg w-[11.25rem]">
         <div
           role="group"
           aria-labelledby="checkbox-group"
           className="flex flex-col"
         >
-          <label className="pb-4">
+          <label className="pb-4 cursor-pointer">
             <input type="checkbox" name="selected" value="draft" />
             <span className="pl-3.5">Draft</span>
           </label>
-          <label className="pb-4">
+          <label className="pb-4 cursor-pointer">
             <input type="checkbox" name="selected" value="pending" />
             <span className="pl-3.5">Pending</span>
           </label>
-          <label>
+          <label className="cursor-pointer">
             <input type="checkbox" name="selected" value="paid" />
             <span className="pl-3.5">Paid</span>
           </label>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import Avatar from "../assets/img/mushroom.jpeg";
 
 function Header() {
   return (
-    <div className="fixed z-10 flex justify-between items-center bg-draftWidget h-17.5 w-full md:h-20 xl:w-[6.4375rem] xl:flex-col xl:rounded-r-[1.75rem] xl:h-screen">
+    <div className="fixed z-50 flex justify-between items-center bg-draftWidget h-17.5 w-full md:h-20 xl:w-[6.4375rem] xl:flex-col xl:rounded-r-[1.75rem] xl:h-screen">
       <div className="h-full xl:h-auto xl:w-full">
         <img src={HeaderIcon} className="h-full xl:w-full" alt="icon" />
       </div>
